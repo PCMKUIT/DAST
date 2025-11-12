25c23d28a3e6534ceb08cafd8d3f1f24dc45fca4
PCMKUIT <145355564+PCMKUIT@users.noreply.github.com>
2025-11-12 13:38:27 +0700

    Delete tools/zap_run_example.sh

--- tools/zap_run_example.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Example invocation -> scans OWASP Juice Shop container if you want a safe target
-# Start Juice Shop in docker (optional)
-# docker run --rm -d -p 3000:3000 bkimminich/juice-shop
-
-
-# Then run scan against http://host.docker.internal:3000 or http://localhost:3000
-# Example:
-./tools/zap_scan.sh http://host.docker.internal:3000 report/zap_juice_shop.html
